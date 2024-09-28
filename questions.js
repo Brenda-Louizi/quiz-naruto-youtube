b82b2b6f1aeedc367dea69396765618857f33d96
Your Name <brendalouizimarinhodesouza@gmail.com>
2024-09-28 11:22:57 -0300

    adicionando os as pastas

---
+++ questions.js
@@ -0,0 +1,78 @@
+export default[
+    {
+        question: "Qual é o nome do professor de Naruto no time 7?",
+        answers: [
+
+            { option: "Kakashi Hatake ", correct: true},
+            { option: "Iruka Umino", correct: false},
+            { option: "Jiraiya", correct: false},
+
+            ],     
+
+    },
+    {
+        question: "Qual é o jutsu que Naruto usa para se transformar em outras pessoas?",
+        answers: [
+
+            { option: "Kage Bunshin no Jutsu  ", correct: false},
+            { option: "Rasengan ", correct: false},
+            { option: "Henge no Jutsu", correct: true},
+
+            ],     
+
+    },
+    {
+        question: "Quem é o líder da Akatsuki no início da série?",
+        answers: [
+
+            { option: "Orochimaru   ", correct: false},
+            { option: "Pain (Nagato)  ", correct: true},
+            { option: "Zetsu ", correct: false},
+
+            ],     
+
+    },
+    {
+        question: "Qual é o nome do clã de Sasuke?",
+        answers: [
+
+            { option: "Clã Yamanaka ", correct: false},
+            { option: "Clã Nara  ", correct: false},
+            { option: "Clã Uchiha ", correct: true},
+
+            ],     
+
+    },
+    {
+        question: "Qual é a besta de nove caudas que está selada dentro de Naruto?",
+        answers: [
+
+            { option: "Kurama   ", correct: true},
+            { option: "Shukaku  ", correct: false},
+            { option: "Gyūki ", correct: false},
+
+            ],     
+
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
